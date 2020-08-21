d0bb72bd869835516028cc044d52a9c319253c91
Diego <zinnadiego@gmail.com>
2020-08-21 12:32:20 -0300

    modificaciones en gracias

--- gracias.php
+++ gracias.php
@@ -4,11 +4,20 @@
 	<title>gracias</title>
 </head>
 <body>
-	<p>Pago gracias</p>
+	<p>Gracias por su compra</p>
 	<?php
-		echo "<pre>";
-		var_dump($_GET);
-		echo "</pre>";
+
+  echo "<p>collection_id: ".$_GET["collection_id"]."</p>";
+  echo "<p>collection_status: ".$_GET["collection_status"]."</p>";
+  echo "<p>external_reference: ".$_GET["external_reference"]."</p>";
+  echo "<p>payment_type: ".$_GET["payment_type"]."</p>";
+  echo "<p>merchant_order_id: ".$_GET["merchant_order_id"]."</p>";
+  echo "<p>preference_id: ".$_GET["preference_id"]."</p>";
+  echo "<p>site_id: ".$_GET["site_id"]."</p>";
+  echo "<p>processing_mode: ".$_GET["processing_mode"]."</p>";
+  echo "<p>merchant_account_id: ".$_GET["merchant_account_id"]."</p>";
+
+
 	?>
 </body>
 </html>
