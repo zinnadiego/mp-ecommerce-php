971ec9f20b49b1f268cb5a2e5d85ca1e5ddee111
Diego <zinnadiego@gmail.com>
2020-08-21 11:24:14 -0300

    modificaciones webhook

--- webhook_mp.php
+++ webhook_mp.php
@@ -1,4 +1,32 @@
 <?php
-mail('zinnadiego@gmail.com', 'test', 'test');
+
+require_once 'vendor/autoload.php';
+
+
+    MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+
+    switch($_POST["type"]) {
+        case "payment":
+            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+            break;
+        case "plan":
+            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+            break;
+        case "subscription":
+            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+            break;
+        case "invoice":
+            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+            break;
+    }
+
+
+$logFile = fopen("log.txt", 'a') or die("Error creando archivo");
+fwrite($logFile, $payment);
+fclose($logFile);
+
+
+ob_clean();
+header("HTTP/1.1 200 OK");
 
 ?>
